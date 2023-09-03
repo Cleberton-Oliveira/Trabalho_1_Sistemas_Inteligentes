cc4b3adb822289ab5853c88b88229ff96c897bd8
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2023-09-03 15:40:16 -0300

    added passos

--- eightPuzzle.mjs
+++ eightPuzzle.mjs
@@ -6,6 +6,7 @@ export default class EightPuzzle {
     constructor(estadoInicial) {
         this.estadoInicial = estadoInicial;
         this.estadoFinal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]];
+        this.passos = 0;
     }
 
     resolve() {
